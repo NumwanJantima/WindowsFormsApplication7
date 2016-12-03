9cb5d53def561e8c5f2718823514d493c0d70867
NumwanJantima <Jantima.y@kkumail.com>
2016-12-03 19:05:09 +0700

    Numwan Projects

---
+++ WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtBinary = new System.Windows.Forms.TextBox();
+            this.txtOctal = new System.Windows.Forms.TextBox();
+            this.txtDecimal = new System.Windows.Forms.TextBox();
+            this.txtHex = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(87, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "โปรแกรมแปลงเลขฐาน";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(42, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ใส่ตัวเลข";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(36, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "เลขฐาน 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.Location = new System.Drawing.Point(36, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "เลขฐาน 8";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.Location = new System.Drawing.Point(27, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "เลขฐาน 10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label6.Location = new System.Drawing.Point(27, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "เลขฐาน 16";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(125, 64);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(133, 20);
+            this.txtInput.TabIndex = 6;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnConvert.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnConvert.Location = new System.Drawing.Point(94, 99);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(183, 36);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "แปลงเลข";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtBinary
+            // 
+            this.txtBinary.Location = new System.Drawing.Point(125, 158);
+            this.txtBinary.Name = "txtBinary";
+            this.txtBinary.Size = new System.Drawing.Size(133, 20);
+            this.txtBinary.TabIndex = 8;
+            // 
+            // txtOctal
+            // 
+            this.txtOctal.Location = new System.Drawing.Point(125, 195);
+            this.txtOctal.Name = "txtOctal";
+            this.txtOctal.Size = new System.Drawing.Size(133, 20);
+            this.txtOctal.TabIndex = 9;
+            // 
+            // txtDecimal
+            // 
+            this.txtDecimal.Location = new System.Drawing.Point(125, 233);
+            this.txtDecimal.Name = "txtDecimal";
+            this.txtDecimal.Size = new System.Drawing.Size(133, 20);
+            this.txtDecimal.TabIndex = 10;
+            // 
+            // txtHex
+            // 
+            this.txtHex.Location = new System.Drawing.Point(125, 269);
+            this.txtHex.Name = "txtHex";
+            this.txtHex.Size = new System.Drawing.Size(133, 20);
+            this.txtHex.TabIndex = 11;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnClear.ForeColor = System.Drawing.Color.Red;
+            this.btnClear.Location = new System.Drawing.Point(39, 326);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(97, 32);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "ลบ";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnExit.ForeColor = System.Drawing.Color.Red;
+            this.btnExit.Location = new System.Drawing.Point(165, 322);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(153, 36);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "ออกจากโปรแกรม";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(355, 385);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtHex);
+            this.Controls.Add(this.txtDecimal);
+            this.Controls.Add(this.txtOctal);
+            this.Controls.Add(this.txtBinary);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "โปรแกรมแปลงเลขฐาน";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtBinary;
+        private System.Windows.Forms.TextBox txtOctal;
+        private System.Windows.Forms.TextBox txtDecimal;
+        private System.Windows.Forms.TextBox txtHex;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        //ปุ่มแปลงเลขฐาน//
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            int Num = int.Parse(txtInput.Text);           //ประกาศตัวแปรรับค่าตัวเลขเข้ามา//
+            txtBinary.Text = Convert.ToString(Num, 2);    //แปลงเลขที่รับเข้ามาเป็นเลขฐาน 2//
+            txtOctal.Text = Convert.ToString(Num, 8);     //แปลงเลขที่รับเข้ามาเป็นเลขฐาน 8//
+            txtDecimal.Text = Num.ToString();             //แปลงเลขที่รับเข้ามาเป็นเลขฐาน 10//
+            txtHex.Text = Convert.ToString(Num, 16);      //แปลงเลขที่รับเข้ามาเป็นเลขฐาน 16//
+        }
+        //ปุ่มลบข้อมูลใน textbox ทั้งหมด//
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+            txtBinary.Clear();
+            txtOctal.Clear();
+            txtDecimal.Clear();
+            txtHex.Clear();
+        }
+        //ปุ่มออกจากโปรแกรม//
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
